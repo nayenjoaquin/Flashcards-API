7f5f3af164b30b2a8ab79f165a35b7f11834ec78
nayenjoaquin <73484502+nayenjoaquin@users.noreply.github.com>
2025-06-17 12:31:33 -0400

    added remove from saved deck

--- src/save-decks/controller.ts
+++ src/save-decks/controller.ts
@@ -111,3 +111,60 @@ export const getMyDecks = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const deleteSaved = async (req: Request, res: Response) => {
+    const { deck_id } = req.params;
+    const authHeader = req.headers.authorization;
+    let user = null;
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
+        try{
+            const {id, created_at, updated_at, username, email, password} = verifyJWT(token) as User;
+            user = {
+                id,
+                created_at,
+                updated_at,
+                username,
+                email,
+                password
+            };
+        }catch(err){
+            console.error('Invalid auth token:', err);
+            res.status(401).send({
+                error: 'Unauthorized',
+                details: 'Invalid token'
+            });
+            return;
+        }
+    }
+    const {error, value} = uuidSchema.validate(deck_id);
+    if (error) {
+        res.status(400).send({
+            error: 'Invalid deck ID, please provide a valid UUID',
+            details: error.details
+        });
+        return;
+    }
+    const q = `
+    DELETE FROM saved
+    WHERE user_id = '${user?.id}' AND deck_id = '${value}';
+    `;
+
+    try{
+        const result = await pool.query(q);
+        if (result.rowCount === 0) {
+            res.status(404).send({
+                error: 'Deck not found',
+                details: 'No saved deck found for the user'
+            });
+            return;
+        }
+        res.status(204).send();
+    }catch (error: any) {
+        console.error('Error forgetting deck:', error);
+        res.status(500).send({
+            error: 'Failed to forget deck',
+            details: error
+        });
+    }
+};
--- src/save-decks/router.ts
+++ src/save-decks/router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getMyDecks, saveDeck } from "./controller";
+import { deleteSaved, getMyDecks, saveDeck } from "./controller";
 
 
 const router = Router();
 
 router.post('/:deck_id', saveDeck);
+router.delete('/:deck_id', deleteSaved);
 router.get('/', getMyDecks);
 
 
