b43ef9cabef77038f1f9eca30407b9a1e1e24cb0
nayenjoaquin <73484502+nayenjoaquin@users.noreply.github.com>
2025-07-31 09:07:01 -0400

    fixed search deckv controller to require authorization token

--- src/decks/search/controller.ts
+++ src/decks/search/controller.ts
@@ -2,30 +2,36 @@ import { Request, Response } from "express";
 import { buildDeckQuery } from "../../shared/queries";
 import { pool } from "../../shared/db";
 import { error } from "console";
+import { verifyAuthorization } from "../../shared/utils";
 
-export const searchDeck = async ( req: Request, res: Response): Promise<void> => {
+export const searchDeck = async ( req: Request, res: Response): Promise<Response> => {
     const {search} = req.query;
+    const user = verifyAuthorization(req, res);
+    if (!user) return res.status(401).send({
+        error: 'Unauthorized',
+        details: 'You must be logged in to access this resource'
+    });
 
     const q = `
-        ${buildDeckQuery('deck')}
+        ${buildDeckQuery('deck', user!.id)}
         WHERE LOWER(d.name) LIKE LOWER('%${search}%')
     `;
-    console.log(q);
-    
 
     try{
         const result = await pool.query(q);
         if (result.rows.length==0){
-            res.status(404).send({
+            return res.status(404).send({
                 error: 'No deck matches the provided search key'
             })
-            return;
         }
 
-        res.send(result.rows);
+        return res.send(result.rows);
 
     }catch(err){
-        console.error(err);
+        return res.status(500).send({
+            error: 'Internal server error',
+            details: err
+        })
         
     }
     
