8fe8726a6ef238401da12693963c57d7559e9ce4
nayenjoaquin <73484502+nayenjoaquin@users.noreply.github.com>
2025-08-12 20:34:55 -0400

    updated default port

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
